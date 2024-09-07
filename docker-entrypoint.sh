2bb3e48fda93c8a51a9303b7276ede54e5b19866
MikhailTo <toshkin.mikhail@yandex.ru>
2024-09-07 10:49:52 +0300

    "Refactored docker-entrypoint.sh script: removed comments, reordered logic, and added echo statements for debugging"

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,28 +1,22 @@
 #!/usr/bin/bash
-# /wait
+echo "Запуск скрипта docker-entrypoint.sh"
 
+echo "Создание директории для миграций"
 mkdir -p /usr/src/app/migrations/versions
 
-# Проверка состояния базы данных и миграций
+echo "Проверка состояния базы данных и миграций"
 if ! alembic current 2>/dev/null | grep -q "head"; then
-    echo "Resetting migrations..."
-    # Удаление таблицы alembic_version, если она существует
-    #psql -U $POSTGRES_USER -d $POSTGRES_DB -c "DROP TABLE IF EXISTS alembic_version;"
-    # Удаление существующих файлов миграций
+    echo "Сброс миграций..."
+    echo "Удаление существующих файлов миграций"
     rm -f /usr/src/app/migrations/versions/*.py
-    # Создание новой миграции
+    echo "Создание новой миграции"
     alembic revision --autogenerate -m "Initial migration"
-    # Применение миграции
+    echo "Применение миграции"
     alembic upgrade head
 else
-    # If alembic_version is valid, just upgrade to head
+    echo "Обнаружена действительная версия alembic, обновление до последней версии"
     alembic upgrade head
 fi
 
-# if [ ! -f /usr/src/app/migrations/versions/*.py ]; then
-#     alembic revision --autogenerate -m "Initial migration"
-#     alembic upgrade head
-# else
-#     alembic upgrade head
-# fi
+echo "Запуск сервера uvicorn"
 uvicorn app.main:app --host 0.0.0.0 --port 8000
