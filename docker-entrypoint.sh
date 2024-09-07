efa6d85a85c8ee1f7326d9645be7544d96fdd8db
MikhailTo <toshkin.mikhail@yandex.ru>
2024-09-07 08:21:43 +0300

    "Uncomment alembic commands and remove comment '# /wait' in docker-entrypoint.sh"

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 # /wait
-# alembic init migrations
-# alembic revision --autogenerate -m "Initial migration"
-# alembic upgrade head
+alembic init migrations
+alembic revision --autogenerate -m "Initial migration"
+alembic upgrade head
 uvicorn app.main:app --host 0.0.0.0 --port 8000
