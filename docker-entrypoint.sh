05940f9381d13526b16ab41af73c19e263081757
MikhailTo <toshkin.mikhail@yandex.ru>
2024-09-07 08:27:37 +0300

    "Added conditional alembic initialization and updated README with migration commands"

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/bash
 # /wait
-alembic init migrations
-alembic revision --autogenerate -m "Initial migration"
-alembic upgrade head
+
+if [ ! -f /usr/src/app/migrations/versions/*.py ]; then
+    alembic init migrations
+    alembic revision --autogenerate -m "Initial migration"
+    alembic upgrade head
+else
+    alembic upgrade head
+fi
 uvicorn app.main:app --host 0.0.0.0 --port 8000
