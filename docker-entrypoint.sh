1d68500837f27a0f01a0f6fd3d48993e8008ba77
MikhailTo <toshkin.mikhail@yandex.ru>
2024-09-07 10:38:56 +0300

    "Update docker-entrypoint.sh and migrations/README files: modify migration script to reset and recreate migrations, and add detailed instructions to README file"

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/bash
 # /wait
 
-mkdir -p /usr/src/app/migrations/versions
-ls -la /usr/src/app/migrations
+#mkdir -p /usr/src/app/migrations/versions
 
-# Check if alembic_version table exists and has valid data
+# Проверка состояния базы данных и миграций
 if ! alembic current 2>/dev/null | grep -q "head"; then
-    # If not, remove existing migration files and recreate
+    echo "Resetting migrations..."
+    # Удаление таблицы alembic_version, если она существует
+    psql -U $POSTGRES_USER -d $POSTGRES_DB -c "DROP TABLE IF EXISTS alembic_version;"
+    # Удаление существующих файлов миграций
     rm -f /usr/src/app/migrations/versions/*.py
+    # Создание новой миграции
     alembic revision --autogenerate -m "Initial migration"
+    # Применение миграции
     alembic upgrade head
 else
     # If alembic_version is valid, just upgrade to head
