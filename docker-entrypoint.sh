75f441738a6a871fa5e206b2144b1f7c2d4395f5
MikhailTo <toshkin.mikhail@yandex.ru>
2024-09-07 08:58:25 +0300

    "Modified docker-entrypoint.sh to change ls command to list /usr/src/app instead of /usr/src/app/migrations/versions/"

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 # /wait
-ls -la /usr/src/app/migrations/versions/
+ls -la /usr/src/app/
 if [ ! -f /usr/src/app/migrations/versions/*.py ]; then
     alembic init migrations
     alembic revision --autogenerate -m "Initial migration"
