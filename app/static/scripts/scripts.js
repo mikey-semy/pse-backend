7f09a20f0191baf2e28809850c7676834c434f5f
MikhailTo <toshkin.mikhail@yandex.ru>
2024-09-07 09:37:31 +0300

    "Remove create bookmark functionality and button"

--- app/static/scripts/scripts.js
+++ app/static/scripts/scripts.js
@@ -5,13 +5,3 @@ document.getElementById('copyBookmarklet').addEventListener('click', function() 
         alert('Bookmarklet code copied to clipboard!');
     });
 });
-
-document.getElementById('createBookmark').addEventListener('click', function() {
-    if (window.sidebar && window.sidebar.addPanel) { // Firefox
-        window.sidebar.addPanel('PSE Bookmarklet', bookmarkletCode, '');
-    } else if (window.external && ('AddFavorite' in window.external)) { // IE
-        window.external.AddFavorite(bookmarkletCode, 'PSE Bookmarklet');
-    } else { // Chrome, Safari, etc.
-        alert('Please press ' + (navigator.userAgent.toLowerCase().indexOf('mac') != -1 ? 'Command/Cmd' : 'CTRL') + ' + D to bookmark this page.');
-    }
-});
