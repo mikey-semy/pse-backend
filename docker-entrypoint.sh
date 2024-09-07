d96670d38ae6687bf3d10a815de6f01348270290
MikhailTo <toshkin.mikhail@yandex.ru>
2024-09-07 10:43:05 +0300

    "Comment out psql command to drop alembic_version table and update mkdir command to create migrations/versions directory"

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 # /wait
 
-#mkdir -p /usr/src/app/migrations/versions
+mkdir -p /usr/src/app/migrations/versions
 
 # Проверка состояния базы данных и миграций
 if ! alembic current 2>/dev/null | grep -q "head"; then
     echo "Resetting migrations..."
     # Удаление таблицы alembic_version, если она существует
-    psql -U $POSTGRES_USER -d $POSTGRES_DB -c "DROP TABLE IF EXISTS alembic_version;"
+    #psql -U $POSTGRES_USER -d $POSTGRES_DB -c "DROP TABLE IF EXISTS alembic_version;"
     # Удаление существующих файлов миграций
     rm -f /usr/src/app/migrations/versions/*.py
     # Создание новой миграции
