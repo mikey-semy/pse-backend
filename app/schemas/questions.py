7e2b2b777c0ab54b8ceeb4c89e2ab764ff8ff23d
Mike <34812500+mikey-semy@users.noreply.github.com>
2024-09-09 09:59:53 +0000

    fix schema of questions

--- app/schemas/questions.py
+++ app/schemas/questions.py
@@ -1,12 +1,19 @@
-from typing import List, Optional
+from typing import List, Optional, Union
 from app.schemas.base import BaseSchema
+from pydantic import field_validator
 
 class QuestionSchema(BaseSchema):
     ''' This is the base class for post-related schemas. '''
     id: Optional[int] = None
     question_text: str
-    answers: List[str]
-    correct_answers: List[str]
+    answers: Union[str, List[str]]
+    correct_answers: Union[str, List[str]]
+    
+    @field_validator('answers', 'correct_answers', pre=True)
+    def ensure_list(cls, v):
+        if isinstance(v, str):
+            return [v]
+        return v
     
     class Config:
         from_attributes = True
