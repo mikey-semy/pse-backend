b044dda85a142ddffb0a1aa49f1df0d955d30ce2
MikhailTo <toshkin.mikhail@yandex.ru>
2024-09-09 06:10:09 +0300

    "Refactor config.py, session.py, and env.py: removed EngineModel and SessionModel, simplified DatabaseModel, and updated related code in session.py and env.py to use the new DatabaseModel."

--- app/core/config.py
+++ app/core/config.py
@@ -1,83 +1,29 @@
-from typing import Dict, Optional, Any
+
 import pprint
-import urllib.parse
-from pydantic import BaseModel, SecretStr, Field
+from pydantic import BaseModel
 from pydantic_settings import (
     BaseSettings,
     SettingsConfigDict,
 )
 from app.const import env_path
-class EngineModel(BaseModel):
-
-    echo:   bool = True
-
-    @property
-    def params(self) -> Dict[str, bool]:
-        return {"echo": self.echo}
-
-class SessionModel(BaseModel):
-
-    autocommit:         bool = False
-    autoflush:          bool = False
-    expire_on_commit:   bool = False
-
-    @property
-    def params(self) -> Dict[str, bool]:
-        return {
-            "autocommit": self.autocommit,
-            "autoflush": self.autoflush,
-            "expire_on_commit": self.expire_on_commit
-        }
 class DatabaseModel(BaseModel):
 
-    engine: EngineModel = EngineModel()
-    session: SessionModel = SessionModel()
-
-    env: str = Field(default="dev")
-    dialect: str = Field(default="sqlite")
-    drivername: str = Field(default="aiosqlite")
-    username: Optional[str] = None
-    password: Optional[SecretStr] = None
-    host: Optional[str] = None
-    port: Optional[int] = None
-    name: str = Field(default="./database_pse.db")
-
-    @property
-    def params(self) -> Dict[str, str]:
-        if self.env == "dev":
-            return {
-                "drivername": f"{self.dialect}+{self.drivername}",
-                "database": self.name
-            }
-        elif self.env == "prod":
-            return {
-                "drivername": f"{self.dialect}+{self.drivername}",
-                "username": self.username,
-                "password": urllib.parse.quote_plus(self.password.get_secret_value()),
-                "host": self.host,
-                "port": self.port,
-                "database": self.name
-            }
+    dsn: str
 
 class Settings(BaseSettings):
 
     db: DatabaseModel = DatabaseModel()
-    db_params: Dict[str, Any] = Field(default_factory=dict)
-    model_config = SettingsConfigDict(
 
+    model_config = SettingsConfigDict(
         env_file=env_path,
         env_file_encoding="utf-8",
         env_nested_delimiter="__",
-
         extra='allow'
     )
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.db_params = {k: v for k, v in self.model_dump().items() if not isinstance(v, dict) and k != 'env'}
+
 config = Settings()
 
 print("=============DB_SETTINGS=============")
 pp = pprint.PrettyPrinter(indent=4)
-# pp.pprint(Settings().model_dump())
-pp.pprint(Settings().db_params)
+pp.pprint(config.model_dump())
 print("==================================")
