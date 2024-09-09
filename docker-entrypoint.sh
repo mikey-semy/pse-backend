cce1c1a873c1dff346752ee2a542c34e586445c8
Mike <34812500+mikey-semy@users.noreply.github.com>
2024-09-09 05:00:47 +0000

    initial migrations

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,6 +17,10 @@ echo "Запуск скрипта docker-entrypoint.sh"
 #     echo "Обнаружена действительная версия alembic, обновление до последней версии"
 #     alembic upgrade head
 # fi
+echo "Удаление существующих файлов миграций"
+rm -f /usr/src/app/migrations/versions/*.py
+echo "Создание новой миграции"
+alembic revision --autogenerate -m "Initial migration"
 echo "Применение миграции"
 alembic upgrade head
 echo "Запуск сервера uvicorn"
