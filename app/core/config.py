dbf7566edd33b550403bc883b2bc5db4b848d2be
Mike <34812500+mikey-semy@users.noreply.github.com>
2024-09-09 04:37:31 +0000

    add default dsn sqlite in config

--- app/core/config.py
+++ app/core/config.py
@@ -9,7 +9,7 @@ from app.const import env_path
 
 class Settings(BaseSettings):
 
-    dsn: str = Field(default=None)
+    dsn: str = Field(default="sqlite+aiosqlite:///./database_pse.db")
 
     model_config = SettingsConfigDict(
         env_file=env_path,
