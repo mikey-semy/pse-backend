1090583062d5f0f917b71979f3cae0e0f50cfd2d
Mike <34812500+mikey-semy@users.noreply.github.com>
2024-09-09 04:43:59 +0000

    alembic upgrade head in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,6 +17,7 @@ echo "Запуск скрипта docker-entrypoint.sh"
 #     echo "Обнаружена действительная версия alembic, обновление до последней версии"
 #     alembic upgrade head
 # fi
-
+echo "Применение миграции"
+alembic upgrade head
 echo "Запуск сервера uvicorn"
 uvicorn app.main:app --host 0.0.0.0 --port 8000
