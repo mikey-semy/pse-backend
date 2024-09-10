cf70408887aa1faaad1d24a6d4494bd5f43dae6f
Mike <34812500+mikey-semy@users.noreply.github.com>
2024-09-10 04:13:05 +0000

    rename and remove folders

--- app/data/questions.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open('app/data/questions.json', 'r', encoding='utf-8') as file:
-    data = json.load(file)
-
-element = json.loads(list(data[0].values())[0])
-print(element["questionText"])
