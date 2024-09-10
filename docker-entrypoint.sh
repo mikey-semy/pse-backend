f34ac16ba1dd605b7acce9086ef20dbfd451bdab
Mike <34812500+mikey-semy@users.noreply.github.com>
2024-09-10 04:21:44 +0000

    remove comments

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,26 +1,5 @@
 #!/usr/bin/bash
 echo "Запуск скрипта docker-entrypoint.sh"
-
-# echo "Создание директории для миграций"
-# mkdir -p /usr/src/app/migrations/versions
-
-# echo "Проверка состояния базы данных и миграций"
-# if ! alembic current 2>/dev/null | grep -q "head"; then
-#     echo "Сброс миграций..."
-#     echo "Удаление существующих файлов миграций"
-#     rm -f /usr/src/app/migrations/versions/*.py
-#     echo "Создание новой миграции"
-#     alembic revision --autogenerate -m "Initial migration"
-#     echo "Применение миграции"
-#     alembic upgrade head
-# else
-#     echo "Обнаружена действительная версия alembic, обновление до последней версии"
-#     alembic upgrade head
-# fi
-#echo "Удаление существующих файлов миграций"
-#rm -f /usr/src/app/migrations/versions/*.py
-#echo "Создание новой миграции"
-#alembic revision --autogenerate -m "Initial migration"
 echo "Применение миграции"
 alembic upgrade head
 echo "Запуск сервера uvicorn"
