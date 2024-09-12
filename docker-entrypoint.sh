7d2e96479ed998eb421ce9cc177d877baf3e8655
Mike <34812500+mikey-semy@users.noreply.github.com>
2024-09-12 12:40:13 +0000

    proxy headers

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,4 @@ echo "Запуск скрипта docker-entrypoint.sh"
 echo "Применение миграции"
 alembic upgrade head
 echo "Запуск сервера uvicorn"
-uvicorn app.main:app --host 0.0.0.0 --port 8000
+uvicorn app.main:app --host 0.0.0.0 --port 8000 --proxy-headers
