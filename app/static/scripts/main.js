76b1184adfde1f0bf2a6a01422187fae92d22ef5
Mike <34812500+mikey-semy@users.noreply.github.com>
2024-09-13 11:37:13 +0000

    templates: add base, index, head, header, footer, back-to-top; static: add styles and scripts for header (active) and back-to-top.

--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -1,1 +1,19 @@
-console.log("I'm working!")
+document.querySelector('.menu-icon').addEventListener('click', function() {
+    document.querySelector('.nav-menu').classList.toggle('active');
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const backToTopButton = document.getElementById('back-to-top');
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 300) {
+            backToTopButton.style.display = 'block';
+        } else {
+            backToTopButton.style.display = 'none';
+        }
+    });
+
+    backToTopButton.addEventListener('click', function() {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    });
+});
