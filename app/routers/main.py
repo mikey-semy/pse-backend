a0ff2f7dc597d945480ccd866bd82f2f9fd857f7
MikhailTo <toshkin.mikhail@yandex.ru>
2024-10-17 18:28:25 +0300

    "Refactored main.py, deleted bookmark.old.js and pse.old.js, updated main.css, and modified base.html and header.html templates."

--- app/routers/main.py
+++ app/routers/main.py
@@ -8,13 +8,46 @@ templates = Jinja2Templates(directory=str(templates_path))
 
 router = APIRouter()
 
+nav_items = [
+        {"route": "manual_search", "icon": "📄", "title": "Ручной поиск"},
+        {"route": "auto_search", "icon": "⚡️", "title": "Автоматический поиск"}
+    ]
+
 @router.get("/", response_class=HTMLResponse)
 async def homepage(request: Request):
+    
+    
     context = {
         "title": "PSE",
-        }
+        "nav_items": nav_items
+    }
+
     return templates.TemplateResponse(
         request=request,
         name="index.html",
+        context=context
+    )
+
+@router.get("/auto_search", response_class=HTMLResponse)
+async def auto_search(request: Request):
+    context = {
+        "title": "Автоматический поиск",
+        "nav_items": nav_items
+    }
+    return templates.TemplateResponse(
+        request=request,
+        name="auto_search.html",
+        context=context
+    )
+
+@router.get("/manual_search", response_class=HTMLResponse)
+async def manual_search(request: Request):
+    context = {
+        "title": "Ручной поиск",
+        "nav_items": nav_items
+    }
+    return templates.TemplateResponse(
+        request=request,
+        name="manual_search.html",
         context=context
     )
