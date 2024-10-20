fcde9fd38f96ccba22531a76c829b5ec186b7fcb
MikhailTo <toshkin.mikhail@yandex.ru>
2024-10-20 23:19:27 +0300

    "Moved back-to-top button functionality from main.js to back-to-top.html and removed related CSS"

--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -3,19 +3,3 @@ document.addEventListener('DOMContentLoaded', function() {
         document.querySelector('.nav').classList.toggle('active');
     });
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-    const backToTopButton = document.getElementById('back-to-top');
-
-    window.addEventListener('scroll', function() {
-        if (window.scrollY > 300) {
-            backToTopButton.style.display = 'block';
-        } else {
-            backToTopButton.style.display = 'none';
-        }
-    });
-
-    backToTopButton.addEventListener('click', function() {
-        window.scrollTo({top: 0, behavior: 'smooth'});
-    });
-});
