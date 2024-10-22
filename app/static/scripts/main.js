3726acab0c10d2b01bc60bf777d4611c16b5afa5
Mikhail Toshkin <toshkin.mikhail@yandex.ru>
2024-10-22 07:27:29 +0000

    fix: add search functionality and UI improvements
    - Add search input and button to manual search page
    - Implement search functionality to fetch and display search results
    - Add back-to-top button to footer and handle its visibility based on scroll position
    - Update content styles to provide proper spacing
    - Add alert message to inform users about keyboard shortcuts
    - Refactor and optimize JavaScript code for better performance and maintainability

--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -33,3 +33,98 @@ document.addEventListener('DOMContentLoaded', function() {
         window.scrollTo({top: 0, behavior: 'smooth'});
     });
 });
+
+document.getElementById('copyButton').addEventListener('click', function() {
+    fetch('/static/scripts/bookmark.new.js')
+        .then(response => response.text())
+        .then(text => {
+            navigator.clipboard.writeText(text)
+                .then(() => alert('Код скопирован в буфер обмена'))
+                .catch(err => console.error('Ошибка копирования: ', err));
+        })
+        .catch(error => console.error('Ошибка загрузки скрипта: ', error));
+});
+
+document.getElementById('search-button').addEventListener('click', performSearch);
+document.getElementById('search-input').addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        performSearch();
+    }
+});
+
+function performSearch() {
+    const searchQuery = document.getElementById('search-input').value;
+    fetch(`/search?q=${encodeURIComponent(searchQuery)}`)
+        .then(response => response.json())
+        .then(data => {
+            const searchResults = document.getElementById('search-results');
+            searchResults.innerHTML = '';
+
+            if (data.length === 0) {
+                // Если нет результатов, выводим сообщение
+                const noResultsItem = document.createElement('li');
+                noResultsItem.textContent = 'Вопрос не найден.';
+                searchResults.appendChild(noResultsItem);
+            } else {
+                // Если есть результаты, выводим их
+                data.forEach(result => {
+                    const resultItem = document.createElement('li');
+                    resultItem.innerHTML = `
+                        <h3>${result.question_text}</h3>
+
+                        ${result.question_type ? `
+                        <p>Тип вопроса: ${result.question_type}</p>
+                        ` : ''}
+                        
+                        ${result.answers ? `
+                        <p>Ответы:</p>
+                        <ul>
+                        ${Array.isArray(result.answers) ? result.answers.map(answer => `<li>${answer}</li>`).join('') : `<li>${result.answers}</li>`}
+                        </ul>
+                        ` : ''}
+                        
+                        <p>Правильные ответы:</p>
+                        <ul>
+                            ${Array.isArray(result.correct_answers) ? result.correct_answers.map(answer => `<li>${answer}</li>`).join('') : `<li>${result.correct_answers}</li>`}
+                        </ul>
+                        
+                    `;
+                    searchResults.appendChild(resultItem);
+                });
+            }
+        })
+        .catch(error => console.error('Ошибка:', error));
+}
+
+
+    // document.addEventListener('DOMContentLoaded', function() {
+    //     const backToTopButton = document.getElementById('back-to-top');
+    //     console.log('Button found:', backToTopButton); // Проверяем, что кнопка найдена
+    
+    //     window.addEventListener('scroll', function() {
+    //         console.log('Scroll position:', document.scrollY); // Проверяем значение scrollY
+    //         if (window.scrollY > 300) {
+    //             backToTopButton.style.display = 'flex';
+    //             console.log('Button displayed'); // Проверяем, что кнопка отображается
+    //         } else {
+    //             backToTopButton.style.display = 'none';
+    //             console.log('Button hidden'); // Проверяем, что кнопка скрыта
+    //         }
+    //     });
+    
+    //     backToTopButton.addEventListener('click', function() {
+    //         window.scrollTo({top: 0, behavior: 'smooth'});
+    //         console.log('Button clicked'); // Проверяем, что кнопка нажата
+    //     });
+    // });
+
+        // Проверяем, было ли уведомление уже показано
+        if (!localStorage.getItem('alertShown')) {
+            document.getElementById('alert').style.display = 'block';
+        }
+    
+        function closeAlert() {
+            document.getElementById('alert').style.display = 'none';
+            // Сохраняем информацию о том, что уведомление было показано
+            localStorage.setItem('alertShown', 'true');
+        }
